6d6989b7fa30b8b1c47febb07061773fcebfbe96
pazmiguela <mpmrhm2018@gmail.com>
2025-08-28 16:05:44 +0800

    20250828

---
+++ tests/yellowOrBlue2.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('Successfully use the page', async ({ page }) => {
+  await page.goto('https://thelab.boozang.com/yellowOrBlue');
+  await page.getByRole('button', { name: 'Generate Color' }).click();
+  await expect(page.locator('h5.color')).toBeVisible();
+
+  const outputText = (await page.locator('h5.color').textContent())?.trim().toLowerCase();
+  console.log('Raw output text:', outputText);
+
+  if (outputText === "yellow") {
+    await page.locator('button', { hasText: /yellow/i }).click();
+  } else if (outputText === "blue") {
+    await page.locator('button', { hasText: /blue/i }).click();
+  } else {
+    console.log('Neither yellow nor blue text is visible.');
+  }
+
+  await expect(page.getByTestId('result')).toContainText('Success!');
+});
